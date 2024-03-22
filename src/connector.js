0085d8cf6a58b5d63085fee1d17e2c5497c04354
unknown <riyarawal70@gmail.com>
2024-03-22 23:19:55 +0530

    mongo uri

--- src/connector.js
+++ src/connector.js
@@ -1,17 +1,16 @@
 const mongodb = require("mongodb");
-require ("dotenv").config();
+// require ("dotenv").config();
 const mongoURI = process.env.MONGOURL + "bookMovie";
 
 let mongoose = require("mongoose");
 const connectToMongo = async () => {
-
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("connection established with mongodb server online");
-  })
-  .catch((err) => {
-    console.log("error while connection", err);
-  });
-}
+  mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("connection established with mongodb server online");
+    })
+    .catch((err) => {
+      console.log("error while connection", err);
+    });
+};
 exports.connection = connectToMongo;
