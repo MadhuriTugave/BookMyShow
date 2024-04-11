8439cab899521151eca2945d4490b48c270c06ca
unknown <riyarawal70@gmail.com>
2024-04-11 16:47:49 +0530

    error solved

--- src/connector.js
+++ src/connector.js
@@ -1,12 +1,10 @@
 const mongodb = require("mongodb");
-<<<<<<< HEAD
+
 require("dotenv").config();
-=======
+
 // require ("dotenv").config();
 let mongoose = require("mongoose");
->>>>>>> df74e3e7dd56194d7cbf60d468243b3073161201
 const mongoURI = process.env.MONGOURL + "bookMovie";
-
 
 const connectToMongo = async () => {
   mongoose
